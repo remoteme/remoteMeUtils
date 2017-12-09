1206cdc7dde91eb59026a0ed16538de8d864d966
msajdak <maciej@mytimeplan.com>
2017-12-09 00:31:00 +0000

    after move repairing

--- src/main/java/it/sajdak/remoteme/utils/v1/enums/_Const.java
+++ src/main/java/it/sajdak/remoteme/utils/v1/enums/_Const.java
@@ -1,8 +1,6 @@
 package it.sajdak.remoteme.utils.v1.enums;
 
-/**
- * Created by kclyu on 05.12.17.
- */
+
 public class _Const {
     public static final String webrtc_negotation_register = "register";
 
