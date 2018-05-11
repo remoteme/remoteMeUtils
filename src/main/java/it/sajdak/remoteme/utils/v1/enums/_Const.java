e42b60afad494e382648884332d3c5034d071222
remoteme <maciej@mytimeplan.com>
2018-05-11 22:32:41 +0000

    next ready for webpagetokens

--- src/main/java/it/sajdak/remoteme/utils/v1/enums/_Const.java
+++ src/main/java/it/sajdak/remoteme/utils/v1/enums/_Const.java
@@ -1,12 +1,4 @@
 package it.sajdak.remoteme.utils.v1.enums;
 
 
-public class _Const {
-    public static final String webrtc_negotation_register = "register";
 
-    public static final String webrtc_negotation_fromDeviceId = "fromDeviceId";
-    public static final String webrtc_negotation_fromSessionId = "fromSessionId";
-    public static final String webrtc_negotation_targetDeviceId = "targetDeviceId";
-    public static final String webrtc_negotation_targetSessionId = "targetSessionId";
-
-}
