1ef620f718780b9ec28024034393a072d7b8a47d
remoteme <maciej@mytimeplan.com>
2018-09-12 23:08:12 +0000

    add code generator for python

--- src/main/java/org/remoteme/utils/messages/v1/enums/VariableType.java
+++ src/main/java/org/remoteme/utils/messages/v1/enums/VariableType.java
@@ -4,31 +4,39 @@ package org.remoteme.utils.messages.v1.enums;
 import org.remoteme.utils.general.Id_Enum;
 import org.remoteme.utils.general.SparseArrayIdEnum;
 
-import java.util.Collection;
-import java.util.List;
-
 public enum VariableType implements Id_Enum<VariableType> {
-    	BOOLEAN(0), INTEGER(1),TEXT(2),SMALL_INTEGER_3(3),SMALL_INTEGER_2(4),INTEGER_BOOLEAN(5),DOUBLE(6),TEXT_2(7);
+	BOOLEAN(0, "Boolean", "B"), INTEGER(1, "Integer", "I"), TEXT(2, "Text", "T"),
+	SMALL_INTEGER_3(3, "SmallInteger3", "SI3"), SMALL_INTEGER_2(4, "SmallInteger2", "SI2"),
+	INTEGER_BOOLEAN(5, "IntegerBoolean", "IB"), DOUBLE(6, "Double", "D"), TEXT_2(7, "Text2", "T2");
 
 
-		private final int id;
+	private final int id;
+	private final String niceName;
+	private final String niceShortName;
 
-		VariableType(int id) {
-			this.id = id;
-		}
-		public int getId() {
-			return id;
-		}
-
-		static SparseArrayIdEnum<VariableType> array = new SparseArrayIdEnum(VariableType.class);
-
-
-		public static List<VariableType> getList(Collection<Integer> ids) {
-			return Id_Enum.getListInner(ids, array);
-		}
-
-		public static VariableType getById(int id) {
-			return Id_Enum.getListInner(id, array);
-		}
-
+	VariableType(int id, String niceName, String niceShortName) {
+		this.id = id;
+		this.niceName = niceName;
+		this.niceShortName = niceShortName;
 	}
+
+	public String getNiceName() {
+		return niceName;
+	}
+
+	public String getNiceShortName() {
+		return niceShortName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	static SparseArrayIdEnum<VariableType> array = new SparseArrayIdEnum(VariableType.class);
+
+
+	public static VariableType getById(int id) {
+		return Id_Enum.getListInner(id, array);
+	}
+
+}
